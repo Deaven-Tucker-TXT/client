bd11c59cdfe2bc7a1ffe9add6bd1243ac5a2e755
Deaven-Tucker-TXT <44715187+Deaven-Tucker-TXT@users.noreply.github.com>
2022-07-07 15:51:43 -0700

    changes

--- src/components/recordList.js
+++ src/components/recordList.js
@@ -69,7 +69,7 @@ export default function RecordList() {
  // This following section will display the table with the records of individuals.
  return (
   <div>
-    <h3>Record List</h3>
+    <h3>The Fruit of my absolute missery</h3>
     <table className="table table-striped" style={{ marginTop: 20 }}>
       <thead>
         <tr>
